bf5bf75a70087e6908b3ce0a25a4a0f1e162f7e8
Bruno Mota <bmotadev@gmail.com>
2023-02-09 18:28:42 -0300

    Modifying our objects

--- aluno.rb
+++ aluno.rb
@@ -1,7 +1,22 @@
 class Aluno
+    attr_accessor :nome, :telefone, :matricula
+
     def initialize(nome, telefone, matricula)
-        @nome = nome # @variavel é usado para declarar os atributos do objetos
+        @nome = nome
         @telefone = telefone
         @matricula = matricula
-      end
+end
+
+
+# Getter
+def telefone
+    @return @telefone
+end
+
+# Setter
+# Sim! Em Ruby podemos declarar métodos com =
+def telefone = (valor)
+    @telefone = valor
+end
+
 end
