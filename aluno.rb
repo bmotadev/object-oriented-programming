e95099cbea6c286a1288acff491910eed8c26136
Bruno Mota <bmotadev@gmail.com>
2023-02-09 14:54:12 -0300

    Created the student class - added attributes

---
+++ aluno.rb
@@ -0,0 +1,7 @@
+class Aluno
+    def initialize(nome, telefone, matricula)
+        @nome = nome # @variavel é usado para declarar os atributos do objetos
+        @telefone = telefone
+        @matricula = matricula
+      end
+end
